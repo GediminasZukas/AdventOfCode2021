6bed8bd97af08ad81eedc80f37a61d7ed545b734
GediminasZukas <gediminas.zukas@vinted.com>
2022-02-01 07:17:28 +0200

    Apply rubocop auto-correct

--- Day03/day3_solution.rb
+++ Day03/day3_solution.rb
@@ -1,47 +1,49 @@
+# frozen_string_literal: true
+
 class Day3
-	def initialize
-		input = File.readlines("day3_input.txt").map(&:strip)
-		problem_1(input)
-		problem_2(input)
-	end
+  def initialize
+    input = File.readlines('day3_input.txt').map(&:strip)
+    problem_1(input)
+    problem_2(input)
+  end
 
-	def problem_1(input)	
-		half_elements = input.size / 2
-		sums_of_ones = calculate_sum_of_ones_in_each_idx(input)
-		gamma = sums_of_ones.map { |count| count > half_elements ? 1 : 0  }
-		epsilon = gamma.map { |e| e == 1 ? 0 : 1  }
-		puts "problem 1 answer: #{gamma.join.to_i(2) * epsilon.join.to_i(2) }"
-	end	
-	
-	def problem_2(input)
-		oxygen_rating = get_rating(input, '1', '0')
-		scrubber_rating = get_rating(input, '0', '1')
-		puts "problem 2 answer: #{oxygen_rating.join.to_i(2) * scrubber_rating.join.to_i(2)}"
-	end	
+  def problem_1(input)
+    half_elements = input.size / 2
+    sums_of_ones = calculate_sum_of_ones_in_each_idx(input)
+    gamma = sums_of_ones.map { |count| count > half_elements ? 1 : 0 }
+    epsilon = gamma.map { |e| e == 1 ? 0 : 1 }
+    puts "problem 1 answer: #{gamma.join.to_i(2) * epsilon.join.to_i(2)}"
+  end
 
-	def get_rating(input, p2, p3)
-		current_input = input
-		input.first.split("").each_index do |index|
-			break if current_input.size == 1
-			sums_of_ones = calculate_sum_of_ones_in_each_idx(current_input)
-			number = sums_of_ones[index]			
-			half_elements = (current_input.size.to_f / 2.to_f).round().to_i
-			target = number >= half_elements ? p2 : p3
-			current_input = current_input.select { |filterable| filterable.split("")[index] == target }
-			
-		end		
-		current_input
-	end
+  def problem_2(input)
+    oxygen_rating = get_rating(input, '1', '0')
+    scrubber_rating = get_rating(input, '0', '1')
+    puts "problem 2 answer: #{oxygen_rating.join.to_i(2) * scrubber_rating.join.to_i(2)}"
+  end
 
-	def calculate_sum_of_ones_in_each_idx(input)
-		array = Array.new(input.first.split("").size, 0)	
-		input.each do |number_char|
-			number_char.split("").each_with_index do |char, index|
-				array[index] += 1 if char == '1'
-			end			
-		end	
-		array
-	end
+  def get_rating(input, p2, p3)
+    current_input = input
+    input.first.split('').each_index do |index|
+      break if current_input.size == 1
+
+      sums_of_ones = calculate_sum_of_ones_in_each_idx(current_input)
+      number = sums_of_ones[index]
+      half_elements = (current_input.size.to_f / 2.to_f).round.to_i
+      target = number >= half_elements ? p2 : p3
+      current_input = current_input.select { |filterable| filterable.split('')[index] == target }
+    end
+    current_input
+  end
+
+  def calculate_sum_of_ones_in_each_idx(input)
+    array = Array.new(input.first.split('').size, 0)
+    input.each do |number_char|
+      number_char.split('').each_with_index do |char, index|
+        array[index] += 1 if char == '1'
+      end
+    end
+    array
+  end
 end
 
 Day3.new
--- Day05/day5_solution.rb
+++ Day05/day5_solution.rb
@@ -1,88 +1,90 @@
+# frozen_string_literal: true
+
 class Day5
-	def initialize
-		input = File.readlines("day5_input.txt")
-		problems_solution(input)
-	end	
+  def initialize
+    input = File.readlines('day5_input.txt')
+    problems_solution(input)
+  end
 
-	def problems_solution(input)
-		lines = input
-			.map { |row| row.split(" -> ") }			
-			.map { |array|
-				array[0] = array[0].split(",").map(&:strip).map(&:to_i)
-				array[1] = array[1].split(",").map(&:strip).map(&:to_i)
-				array
-			}	
+  def problems_solution(input)
+    lines = input
+            .map { |row| row.split(' -> ') }
+            .map do |array|
+      array[0] = array[0].split(',').map(&:strip).map(&:to_i)
+      array[1] = array[1].split(',').map(&:strip).map(&:to_i)
+      array
+    end
 
-		valid_overlaps = 0
+    valid_overlaps = 0
 
-		# Hash table of rows (y coordinates). Each row is a hashtable too (x coordinate (key) and amount of occurrences (value))
-		diagram = Hash.new
+    # Hash table of rows (y coordinates). Each row is a hashtable too (x coordinate (key) and amount of occurrences (value))
+    diagram = {}
 
-		lines.each do |line|
-			line_start = line[0]
-			line_end = line[1]	
+    lines.each do |line|
+      line_start = line[0]
+      line_end = line[1]
 
-			if line_start[1] == line_end[1] # horizontal line
-				y = line_start[1]
-				diagram_row = diagram[y] || Hash.new
-				x1 = line_start[0]
-				x2 = line_end[0]
-				min = [x1, x2].min
-				max = [x1, x2].max
-				for i in min..max					
-					times_covered = diagram_row[i] || 0
-					times_covered += 1
-					valid_overlaps += 1 if times_covered == 2					
-					diagram_row.store(i, times_covered)					
-				end
-				diagram.store(y, diagram_row)	
-			elsif line_start[0] == line_end[0]  # vertical line
-				x = line_start[0]	
-				y1 = line_start[1]
-				y2 = line_end[1]
-				min = [y1, y2].min
-				max = [y1, y2].max
-				for i in min..max
-					diagram_row = diagram[i] || Hash.new
-					times_covered = diagram_row[x] || 0
-					times_covered += 1
-					valid_overlaps += 1 if times_covered == 2					
-					diagram_row.store(x, times_covered)
-					diagram.store(i, diagram_row)
-				end				
-			else # diagonal line
-				y1 = line_start[1]
-				y2 = line_end[1]
-				min = [y1, y2].min
-				max = [y1, y2].max
-				x1 = line_start[0]
-				x2 = line_end[0]
-				next_x = -1
-				x_incrementing = false
-				if y1 < y2
-					next_x = x1
-					x_incrementing = true if x1 < x2
-				else
-					next_x = x2
-					x_incrementing = true if x2 < x1
-				end										
-				for i in min..max
-					diagram_row = diagram[i] || Hash.new
-					times_covered = diagram_row[next_x] || 0
-					times_covered += 1
-					valid_overlaps += 1 if times_covered == 2					
-					diagram_row.store(next_x, times_covered)
-					diagram.store(i, diagram_row)
-					if (x_incrementing)
-						next_x = next_x + 1 
-					else 
-						next_x = next_x - 1 
-					end
-				end					
-			end
-		end
-		puts "problem 2 answer: #{valid_overlaps}. Comment diagonal branch to get answer for problem 1." # problem 1 answer - 6311, problem 2 answer - 19929
-	end		
+      if line_start[1] == line_end[1] # horizontal line
+        y = line_start[1]
+        diagram_row = diagram[y] || {}
+        x1 = line_start[0]
+        x2 = line_end[0]
+        min = [x1, x2].min
+        max = [x1, x2].max
+        (min..max).each do |i|
+          times_covered = diagram_row[i] || 0
+          times_covered += 1
+          valid_overlaps += 1 if times_covered == 2
+          diagram_row.store(i, times_covered)
+        end
+        diagram.store(y, diagram_row)
+      elsif line_start[0] == line_end[0] # vertical line
+        x = line_start[0]
+        y1 = line_start[1]
+        y2 = line_end[1]
+        min = [y1, y2].min
+        max = [y1, y2].max
+        (min..max).each do |i|
+          diagram_row = diagram[i] || {}
+          times_covered = diagram_row[x] || 0
+          times_covered += 1
+          valid_overlaps += 1 if times_covered == 2
+          diagram_row.store(x, times_covered)
+          diagram.store(i, diagram_row)
+        end
+      else # diagonal line
+        y1 = line_start[1]
+        y2 = line_end[1]
+        min = [y1, y2].min
+        max = [y1, y2].max
+        x1 = line_start[0]
+        x2 = line_end[0]
+        next_x = -1
+        x_incrementing = false
+        if y1 < y2
+          next_x = x1
+          x_incrementing = true if x1 < x2
+        else
+          next_x = x2
+          x_incrementing = true if x2 < x1
+        end
+        (min..max).each do |i|
+          diagram_row = diagram[i] || {}
+          times_covered = diagram_row[next_x] || 0
+          times_covered += 1
+          valid_overlaps += 1 if times_covered == 2
+          diagram_row.store(next_x, times_covered)
+          diagram.store(i, diagram_row)
+          next_x = if x_incrementing
+                     next_x + 1
+                   else
+                     next_x - 1
+                   end
+        end
+      end
+    end
+    puts "problem 2 answer: #{valid_overlaps}. Comment diagonal branch to get answer for problem 1." # problem 1 answer - 6311, problem 2 answer - 19929
+  end
 end
 
 Day5.new
