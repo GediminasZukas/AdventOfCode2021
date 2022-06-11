7159558f8e124d53d135fc8527fff5d5b0081b35
GediminasZukas <gediminas.zukas@vinted.com>
2022-06-11 20:45:49 +0300

    Add day 9 problem 2 solution

--- Day09/day9_solution.rb
+++ Day09/day9_solution.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class Day9
+  $VERBOSE = nil
+  @@basin_size = 0
+  @@visited_coords = []
+
   def initialize
     input = File.readlines('day9_input.txt').map { |line| line.strip.split('').map(&:strip).map(&:to_i) }
-    problem_one_solution(input)
+    low_points_data = problem_one_solution(input)
+    problem_two_solution(input, low_points_data)
   end
 
   def problem_one_solution(input)
@@ -13,8 +18,9 @@ class Day9
         check_for_low_point(low_points, input, point, row_index, column_index)
       end
     end
-    answer = low_points.sum + low_points.size
+    answer = low_points.map { |point_data| point_data.keys.first }.sum + low_points.size
     puts "problem 1 answer: #{answer}" # correct answer 588
+    low_points
   end
 
   def check_for_low_point(low_points, input, point, row_index, column_index)
@@ -31,7 +37,47 @@ class Day9
     unless row_index + 1 >= input.size
       adjacent_locations.push(input[row_index + 1][column_index]) # down
     end
-    low_points.push(point) if adjacent_locations.min > point
+    low_points.push(point => [row_index, column_index]) if adjacent_locations.min > point
+  end
+end
+
+def problem_two_solution(input, low_points_data)
+  basins = []
+  low_points_data.each do |point|
+    @@visited_coords = []
+    @@basin_size = 1
+    start_coordinates = point.values.first
+    traverse_basin(input, start_coordinates)
+    basins.push(@@basin_size)
+  end
+  answer = basins.sort.last(3).inject(:*)
+  puts "problem 2 answer: #{answer}" # correct answer 964712
+end
+
+def traverse_basin(input, coordinates)
+  row = coordinates[0]
+  column = coordinates[1]
+  value = input[row][column]
+
+  if row - 1 >= 0 && input[row - 1][column] < 9 && input[row - 1][column] > value && !@@visited_coords.include?([row - 1, column])
+    @@visited_coords.push([row - 1, column])
+    @@basin_size += 1
+    traverse_basin(input, [row - 1, column]) # go up
+  end
+  if column + 1 < input[row].size && input[row][column + 1] < 9 && input[row][column + 1] > value && !@@visited_coords.include?([row, column + 1])
+    @@visited_coords.push([row, column + 1])
+    @@basin_size += 1
+    traverse_basin(input, [row, column + 1]) # go right
+  end
+  if row + 1 < input.size && input[row + 1][column] < 9 && input[row + 1][column] > value && !@@visited_coords.include?([row + 1, column])
+    @@visited_coords.push([row + 1, column])
+    @@basin_size += 1
+    traverse_basin(input, [row + 1, column]) # go down
+  end
+  if column - 1 >= 0 && input[row][column - 1] < 9 && input[row][column - 1] > value && !@@visited_coords.include?([row, column - 1])
+    @@visited_coords.push([row, column - 1])
+    @@basin_size += 1
+    traverse_basin(input, [row, column - 1]) # go left
   end
 end
 
